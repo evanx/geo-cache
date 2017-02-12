f89b995537377568997b5107c83427fdcb50c4da
Evan Summers <evan.summers@gmail.com>
2017-02-12 14:06:14 +0000

    redeploy script

--- test/redeploy.sh
+++ test/redeploy.sh
@@ -3,4 +3,4 @@ docker build -t geo-cache https://github.com/evanx/geo-cache.git
 docker rm -f `docker ps -q -f name=geo-cache`
 docker run --name geo-cache -d \
   --network=host --restart unless-stopped \
-  -e httpPort=8851 geo-cache
+  -e httpPort=8851 -e NODE_ENV=test geo-cache
