8872ee6dcdb344bafceb286d2ccdbd36ef5a4ddd
Evan Summers <evan.summers@gmail.com>
2017-02-24 16:59:43 +0000

    initial

--- bin/redeploy.sh
+++ bin/redeploy.sh
@@ -2,8 +2,12 @@
 echo "NODE_ENV $NODE_ENV"
 docker build -t geo-cache https://github.com/evanx/geo-cache.git
 docker rm -f `docker ps -q -f name=geo-cache`
-docker run --name geo-cache -d \
+container=`docker run --name geo-cache -d \
   --network=host --restart unless-stopped \
   -e httpPort=8851 \
   -e NODE_ENV=$NODE_ENV \
-  geo-cache
+  geo-cache`
+sleep 1 
+echo "docker logs $container"
+docker logs $container
+
